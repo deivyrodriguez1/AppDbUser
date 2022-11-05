90cbdf433930cbd91d43e4d84b3228dd20316d0c
Deivy Rodriguez <deivy.rodriguez@correo.policia.gov.co>
2022-11-05 12:51:07 -0500

    guardar registros en la base de datos

--- app/src/main/java/com/example/userform/DbList.java
+++ app/src/main/java/com/example/userform/DbList.java
@@ -1,14 +1,40 @@
 package com.example.userform;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class DbList extends AppCompatActivity {
 
+    RecyclerView miVistaRecicladora;
+    UserDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db_list);
+
+        miVistaRecicladora = findViewById(R.id.listaUsuarios);
+        db = UserDatabase.getDatabase(this);
+
+        Bundle misDatos = getIntent().getExtras();
+        User miUsuario = new User();
+        miUsuario.nombre = misDatos.getString("nombre");
+        miUsuario.apellido = misDatos.getString("apellido");
+        miUsuario.peso = Float.parseFloat(misDatos.getString("peso"));
+
+
+
+        //Cargar los datos empleando el DAO
+        //Tenemos que crear un nuevo hilo de ejecución
+
+        new Thread(
+                ()->{
+                    db.userDao().insertarUsuario(miUsuario);
+                }
+        ).start();
+
+
     }
 }
